01f012d2fd1ad698bfd8ce00355442dbb1bc9ea2
Fernando Quintero <ferdanielq@outlook.com>
2018-11-18 16:54:10 -0500

    name changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-import { CourseListingsComponent } from "./modules/course-listings/course-listings.component";
+import { CourseListingsComponent } from "./components/course-listings/course-listings.component";
 
 @NgModule({
   declarations: [CourseListingsComponent],
   imports: [BrowserModule],
   bootstrap: [CourseListingsComponent]
 })
-export class CourseListingsModule {}
+export class AppModule {}
