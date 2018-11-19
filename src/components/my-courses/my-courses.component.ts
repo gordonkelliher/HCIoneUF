995973cf571755d2f0fd6c456c53ad313e5d8ed0
natehooper <natehoop@gmail.com>
2018-11-19 16:58:45 -0500

    ayy

--- src/components/my-courses/my-courses.component.ts
+++ src/components/my-courses/my-courses.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
 import { Course } from "src/models/course";
 
 @Component({
@@ -8,7 +8,8 @@ import { Course } from "src/models/course";
 })
 export class MyCoursesComponent implements OnInit {
 
-  private courses: Course[] = [];
+  // private courses: Course[] = [];
+  @Input() private courses: Course[];
   public filteredCourses: Course[] = [];
 
 
@@ -19,6 +20,19 @@ export class MyCoursesComponent implements OnInit {
   }
 
   public checkCourses() {
+    alert("WAT");
+    alert(this.courses);
+    for (var i = 0; i < this.courses.length; i++) {
+      alert("WAT");
+
+      var course = this.courses[i];
+      alert(course.name);
+      // for (var j = 0; j < course.sections; j++) {
+
+        // this.selection.push(this.studentList[i].id);
+        // console.log(this.selection);
+      // }
+    }
     // this.filteredCourses = [];
     //
     // this.courses.forEach(course => {
