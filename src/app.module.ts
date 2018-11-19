96fc69ed2351c3452ab8ae9a8ee438a6b0ae02ec
natehooper <natehoop@gmail.com>
2018-11-18 21:48:57 -0500

    component

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { CourseListingsComponent } from "./components/course-listings/course-listings.component";
 import { CoursesViewComponent } from './components/courses-view/courses-view.component';
+import { MyCoursesComponent } from './components/my-courses/my-courses.component';
 
 @NgModule({
-  declarations: [CourseListingsComponent, CoursesViewComponent],
+  declarations: [CourseListingsComponent, CoursesViewComponent, MyCoursesComponent],
   imports: [BrowserModule],
   bootstrap: [CoursesViewComponent]
 })
